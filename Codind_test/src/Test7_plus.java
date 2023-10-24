057ebe38581c6aeab08cec77f84a3bafbdcd6f15
yeswon9874 <yeswon9874@gmail.com>
2023-10-24 15:56:01 +0900

    2023/10/24 백준 5597 plus

--- Codind_test/src/Test7_plus.java
+++ Codind_test/src/Test7_plus.java
@@ -5,15 +5,19 @@ public class Test7_plus {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		// 30명  학생중에서
 		int[] arr = new int[31];
 		
+		// 28명의 학생수를 for문 돌려서
 		for(int i=1; i < 29; i++) {
 			arr[sc.nextInt()]++;
 		}
 		sc.close();
 		
 		for(int i=1; i <30; i++) {
+			// 미체줄한 학생이 있으면
 			if(arr[i] == 0) {
+				// 출력
 				System.out.println(i);
 			}
 		}
